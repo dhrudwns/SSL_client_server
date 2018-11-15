3e496f64ddc17062652e5167b95e865f1cd28593
dhrudwns <35209434+dhrudwns@users.noreply.github.com>
2018-11-16 01:00:01 +0900

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -8,73 +8,108 @@
 #define MAX_PACKETLEN 1460
 using namespace std;
 
-int main(int argc, char* argv[])
+int create_socket(int port)
 {
-	char ReadBuffer[MAX_PACKETLEN] = { "\n" }, WriteBuffer[MAX_PACKETLEN] = "HTTP/1.1 200 OK\nContent-Length:5\n\nhello\0";
-	int Readn, Writen;
-	if (argc != 2) {
-		cout << "ex) " << argv[0] << "<Port>" << endl;
-		return -1;
-	}
-
-	WSADATA WsaData;
-
-
-	// initalization
-	if (WSAStartup(MAKEWORD(2, 2), &WsaData) != 0) {
-		cout << "Socket reset error" << endl;
-	}
-
-	SOCKET server = socket(AF_INET, SOCK_STREAM, 0);
-	if (server == SOCKET_ERROR) {
-		cout << "Socket create error" << endl;
-		WSACleanup();
-	}
-
-	sockaddr_in server_addr, client_addr;
+	SOCKET server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	sockaddr_in server_addr;
 	memset(&server_addr, 0, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(atoi(argv[1]));
+	server_addr.sin_port = htons(port);
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-
-
-	if (bind(server, (struct sockaddr *)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)
-	{	
-		cout << "bind error" << endl;
-	}
-	if (listen(server, 5) == SOCKET_ERROR)
+	if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)
 	{
-		cout << "listen error" << endl;
+		cout << "bind error : " << WSAGetLastError() << endl;
+	}
+	if (listen(server_socket, 5) == SOCKET_ERROR)
+	{
+		cout << "listen error : " << WSAGetLastError() << endl;
 	}
 
-	SSL_library_init();				
-	SSL_CTX* ctx = SSL_CTX_new(TLSv1_2_server_method());
-	SSL_CTX_use_certificate_file(ctx, "C:\\Program Files\\SnoopSpy\\certificate\\test.com.crt", SSL_FILETYPE_PEM);
-	SSL_CTX_use_PrivateKey_file(ctx, "C:\\Program Files\\SnoopSpy\\certificate\\test.com.key", SSL_FILETYPE_PEM);
-	while (1)
-	{
-		cout << "1. hi" << endl;
-		int client_addr_size = sizeof(client_addr);
-		SOCKET client = accept(server, (struct sockaddr *)&client_addr, &client_addr_size);
-		cout << "3. hi" << endl;
-		if (client == SOCKET_ERROR) {
-			cout << "accept error" << endl;
-			return -1;
-		} 
-		cout << "2. hi" << endl;
-		SSL *ssl = SSL_new(ctx);
-		SSL_set_fd(ssl, client);
-		SSL_accept(ssl);
+	return server_socket;
+}
+
+void init_openssl()
+{
+	SSL_load_error_strings();
+	OpenSSL_add_ssl_algorithms(); // initalizaiton ssl
+}
+
+void cleanup_openssl()
+{
+	EVP_cleanup(); 
+}
+
+SSL_CTX *create_context()
+{
+	SSL_CTX *ctx = SSL_CTX_new(TLSv1_2_server_method());
+	if (!ctx) {
+		perror("Unable to create SSL context");
+		ERR_print_errors_fp(stderr);
+		exit(EXIT_FAILURE);
+	}
+	return ctx;
+}
+
+void configure_context(SSL_CTX *ctx)
+{
+	// crt load to ctx, success return 1
+	if (SSL_CTX_use_certificate_file(ctx, "C:\\Program Files\\SnoopSpy\\certificate\\test.com.pem", SSL_FILETYPE_PEM) != 1) {
+		ERR_print_errors_fp(stderr);
+		exit(EXIT_FAILURE);
+	}
+	// key load to ctx
+	if (SSL_CTX_use_PrivateKey_file(ctx, "C:\\Program Files\\SnoopSpy\\certificate\\test.com.key", SSL_FILETYPE_PEM) != 1) {
+		ERR_print_errors_fp(stderr);
+		exit(EXIT_FAILURE);
+	}
+}
+int main(int argc, char* argv[])
+{
+	SSL_CTX *ctx;
+	sockaddr_in client_addr;
+	int client_addr_size = sizeof(client_addr);
+	WSADATA wsadata;
+	if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0) {
+		cout << "Socket reset error : " << WSAGetLastError() << endl;
+	}
+
+	init_openssl();
+	ctx = create_context();
+
+	configure_context(ctx);
+
+	SOCKET socket = create_socket(atoi(argv[1]));
+
+	while (1) {
+		SSL *ssl;
+		char *server_hello = "HTTP/1.1 200 OK\nContent-Length: 5\n\nHello\n\0", c_message[MAX_PACKETLEN] = { "\0", };
 		
-		while (SSL_read(ssl, ReadBuffer, sizeof(ReadBuffer)) > 0) {
-			cout << ReadBuffer << endl;
-			memset(ReadBuffer, '\0', sizeof(ReadBuffer));
-			SSL_write(ssl, WriteBuffer, strlen(WriteBuffer));
-			SSL_free(ssl);
+		SOCKET client_socket = accept(socket, (struct sockaddr *)&client_addr, &client_addr_size);
+		if (client_socket == SOCKET_ERROR) {
+			cout << "accept error : " << WSAGetLastError() << endl;
 		}
+		
+		ssl = SSL_new(ctx); // create ssl struct
+		SSL_set_fd(ssl, client_socket); // connect ssl with client
+
+		// handshake, success return 1
+		if (SSL_accept(ssl) <= 0) {
+			cout << "ssl accept error : ";
+			ERR_print_errors_fp(stderr);
+			
+		}
+		else {
+			SSL_read(ssl, c_message, sizeof(c_message));
+			cout << c_message;
+			SSL_write(ssl, server_hello, strlen(server_hello));
+		}
+
+		SSL_free(ssl); // ssl struct cancel
+		closesocket(client_socket);
 	}
+	closesocket(socket);
 	SSL_CTX_free(ctx);
+	cleanup_openssl();
 	WSACleanup();
-	closesocket(server);
 }
